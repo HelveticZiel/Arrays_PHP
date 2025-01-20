aac7626b64b496adab49bf11d17b495ef7a36c62
HelveticZiel <helveticdisplay@gmail.com>
2025-01-20 10:02:52 +0100

    first commit

---
+++ Arrays.php
@@ -0,0 +1,28 @@
+<?php
+
+//1) Crea un array asociativo con los siguientes datos y claves.
+//Muestra los valores del array anterior utilizando foreach. 
+
+echo "<style= <h2> Ejercicio 1 <h2>";
+echo '<h2 style="color: blue; font-family:Helvetica;">Ejercicio 1</h2>';
+$lista = array("Nombre"=>"Sara", "Apellido"=>"Martínez", "edad"=>"23", "ciudad"=>"Barcelona");
+
+$count=1;
+
+foreach ($lista as $x => $y) {
+    echo "Dato". $count. "º:". $y . "<br>";
+    $count ++;
+  }
+
+//2) Muestra los valores del array del ejercicio 1
+// mostrando la clave y el valor utilizando foreach
+
+foreach($lista as $x => $y){
+
+    echo  $x + $y ; 
+
+}
+
+
+
+?>
